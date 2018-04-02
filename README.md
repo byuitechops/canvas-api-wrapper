746d5fb193d0ed546c2cd2c82c402576c5e5d37a
Benjamin Earl <benearl@protonmail.com>
2018-04-02 16:55:43 -0600

    made a bunch more breaking changes

--- README.md
+++ README.md
@@ -2,41 +2,41 @@
 Simplifies the already simple canvas api calls, and handles pagenation
 
 ## Get Started
-##### Install
+#### Install
 ```
 npm i --save canvas-api-wrapper
 ```
-##### Setup
+#### Setup
 ``` javascript
 /* main.js */
-var Canvas = require('canvas-api-wrapper')
-
-var options = {
-	domain:'pathway'
-}
-
-var canvas = Canvas(options)
+var canvas = require('canvas-api-wrapper')
 ```
-``` javascript 
-/* auth.json */
-{
-	"token":"<ACCESS-TOKEN>"
-}
+Authorization:
+```
+canvas.apiToken = '<TOKEN>'
+```
+``` bash
+# Powershell
+$env:CANVAS_API_TOKEN="<TOKEN>"
+# CMD
+set CANVAS_API_TOKEN="<TOKEN>"
+# Linux & Mac
+export CANVAS_API_TOKEN="<TOKEN>"
 ```
 
-##### Use Await
+#### Use Await
 ``` javascript
 let response = await canvas('/api/v1/users/self')
 console.log(response.body.name)
 ```
-##### Use Promises
+#### Use Promises
 ``` javascript
 canvas('/api/v1/users/self')
 	.then(response => {
 		console.log(response.body.name)
 	})
 ```
-##### Use Callbacks
+#### Use Callbacks
 ``` javascript
 canvas('/api/v1/users/self', function(err,response){
 	if(err) return console.error(err);
@@ -63,37 +63,55 @@ canvas.head(url[,options][,callback])
 canvas.delete(url[,options][,callback])
 ```
 
-
+### Default Settings
 ``` javascript
-// The default options
-var options = {
-	// the subdomain under canvas 
-	//  https://<domain>.instructure.com
-	domain: 'byui',
+// the subdomain under canvas 
+//  https://<domain>.instructure.com
+canvas.domain = 'byui';
 
-	// the location of the auth file containing your canvas token
-	// which looks like { "token":<access-token> }
-	authFile: 'auth.json',
+// Scale of 0 to 700, you get filled up to 700 and if you go under
+// 0 then canvas will start sending you 403 (unauthorized) or tell 
+// you that the servers are melting. So when it goes past this number
+// I will halt the requests until it gets filled back to this level
+// Give it a pretty large buffer, it tends to go quite a ways past the
+// buffer before I catch it.
+canvas.rateLimitBuffer = 300;
 
-	// Scale of 0 to 700, you get filled up to 700 and if you go under
-	// 0 then canvas will start sending you 403 (unauthorized) or tell 
-	// you that the servers are melting. So when it goes past this number
-	// I will halt the requests until it gets filled back to this level
-	// Give it a pretty large buffer, it tends to go quite a ways past the
-	// buffer before I catch it.
-	rateLimitBuffer: 300,
+// How many to send synchronously at the same time, the higher this
+// number, the more it will go over your rateLimitBuffer
+canvas.callLimit = 30;
 
-	// How many to send synchronously at the same time, the higher this
-	// number, the more it will go over your rateLimitBuffer
-	callLimit: 30,
+// How much the calls are staggered (milliseconds) especially at the
+// beginning so that it doesn't send the callLimit all at the same time
+canvas.minSendInterval = 10;
 
-	// How much the calls are staggered (milliseconds) especially at the
-	// beginning so that it doesn't send the callLimit all at the same time
-	minSendInterval: 10,
-
-	// After it goes under the rateLimitBuffer, how often to check what the
-	// buffer is at now, this should be pretty high because there will be
-	// a lot of threads checking at the same time.
-	checkStatusInterval: 2000
+// After it goes under the rateLimitBuffer, how often to check what the
+// buffer is at now, this should be pretty high because there will be
+// a lot of threads checking at the same time.
+canvas.checkStatusInterval = 2000;
 }
+```
+
+### Examples
+``` js
+(async () => {
+	var res = await canvas('/api/v1/courses/10698/modules')
+	console.log(`There are ${res.body.length} modules`)
+
+	await canvas.post('/api/v1/courses/10698/modules',{
+		body:{
+			module:{
+				name:"New Module"
+			}
+		}
+	})
+
+	// This query option also comes from got
+	res = await canvas('/api/v1/courses/10698/modules',{
+		query:{
+			search_term:'New Module'
+		}
+	})
+	console.log('Found my new module',res.body)
+})()
 ```
--- main.js
+++ main.js
@@ -2,161 +2,178 @@ const got = require('got')
 const promiseLimit = require('promise-limit')
 const stack = require('callsite')
 const url = require('url')
-const fs = require('fs')
-const path = require('path')
 const util = require('util')
 
-const defaulter = (g,d) => g !== undefined ? g : d
+const settings = {
+  domain:'byui',
+  apiToken: process.env.CANVAS_API_TOKEN || '',
+  rateLimitBuffer: 300,
+  callLimit: 30,
+  minSendInterval: 10,
+  checkStatusInterval: 2000,
+}
 
-/** Simple Canvas API wrapper */
-class Canvas {
-  constructor(origFolder,defaults = {}) {
-    this.authFile = path.join(origFolder,defaults.authFile || 'auth.json')
-    this.baseUrl = `https://${defaults.domain||'byui'}.instructure.com`
-    this.accessToken = this.readAuthFile()
-    this.RateLimitRemaining = 700
-    this.RateLimitBuffer = defaulter(defaults.rateLimitBuffer,300)
-    this.queue = promiseLimit(defaulter(defaults.callLimit,30))
-    this.minSendInterval = defaulter(defaults.minSendInterval,10)
-    this.nextSendTime = Date.now()
-    this.lastOverBuffer = 0
-    this.checkStatusInterval = defaulter(defaults.checkStatusInterval,2000)
-  }
-  readAuthFile(){
-    if(!fs.existsSync(this.authFile)){
-      fs.writeFileSync(this.authFile,'{ "token":"" }')
-      throw new Error(`Please place your auth token in the "token" property of the ${this.authFile} { "token": <ACCESS-TOKEN> }`)
-    }
-    let file = JSON.parse(fs.readFileSync(this.authFile))
-    if(!file.token){
-      throw new Error(`Please place your auth token in the "token" property of the ${this.authFile} { "token": <ACCESS-TOKEN> }`)
-    }
-    return file.token
-  }
-  range(e){
-    return [...Array(e-1).keys()].map(n => n+2)
-  }
-  async call(blame,path, options = {}) {
-    // Don't let the queue build up too high
-    while(this.queue.queue > 40) await new Promise(res => setTimeout(res,500))
-    
-    path = new url.URL(url.resolve(this.baseUrl,path))
-    
-    options = Object.assign({
-      method:'GET',
-      // TODO: should users be able to override json attribute?
-      json:true // canvas always return json
-    },options)
-    
-    options.headers = Object.assign({
-      Authorization: 'Bearer '+this.accessToken
-    },options.headers)
-    
-    let response = await this.queue((async () => {
-      // Make sure our rate limit is not over
-      while(this.RateLimitRemaining < this.RateLimitBuffer){
-        // Display this message if has been at least a second since it was last displayed
-        if(Date.now() - this.lastOverBuffer > this.checkStatusInterval){
-          console.log(`Our rate-limit-remaining (${Number(this.RateLimitRemaining).toFixed(1)}) is below our buffer (${this.RateLimitBuffer}), waiting a sec...`)
-          this.lastOverBuffer = Date.now()
-        }
-        // The waiting a second
-        await new Promise(res => setTimeout(res,this.checkStatusInterval))
-        // See what the situation is now
-        try{
-          let response = await got(url.resolve(this.baseUrl,'/api/v1/users/self'),{
-            // TODO: Is this returning JSON?
-            method: 'HEAD',
-            headers:{
-              Authorization: 'Bearer '+this.accessToken
-            }
-          })
-          this.RateLimitRemaining = response.headers['x-rate-limit-remaining']
-        } catch(e){
-          console.log('We\'re in trouble')
-        }
-        if(this.RateLimitRemaining === undefined){
-          throw new Error("There was no x-rate-limit-remaining header")
-        }
+// I hate global variables, but somehow I need to save info from call to call
+let queue = promiseLimit(settings.callLimit),
+  nextSendTime = Date.now(),
+  lastOverBuffer = 0,
+  baseUrl= `https://${settings.domain}.instructure.com`,
+  rateLimitRemaining = 700
+
+// The center of the universe
+async function call(blame,path, options = {}) {
+  // Don't let the queue build up too high
+  while(queue.queue > 40) await new Promise(res => setTimeout(res,500))
+  
+  // Resolving the path
+  path = new url.URL(url.resolve(baseUrl,path))
+  
+  // Setting up the options
+  options = Object.assign({
+    method:this.method,
+  },options)
+  // Always use json even if the user specifies not to
+  options.json = true
+  // Set the authorization token
+  options.headers = Object.assign({
+    Authorization: 'Bearer '+settings.apiToken,
+  },options.headers)
+  
+  // Make the request (with the timing checks and stuff)
+  let response = await queue(async () => {
+    // Make sure our rate limit is not over
+    while(rateLimitRemaining < settings.rateLimitBuffer){
+      // Display this message if has been at least a second since it was last displayed
+      if(Date.now() - lastOverBuffer > settings.checkStatusInterval){
+        console.log(`Our rate-limit-remaining (${Number(rateLimitRemaining).toFixed(1)}) is below our buffer (${settings.rateLimitBuffer}), waiting a sec...`)
+        lastOverBuffer = Date.now()
       }
-
-      // Make sure our calls are staggered at least a little bit, like 10 milliseconds, so that the first go doesn't spam the server
-      var timeTillSend = this.nextSendTime - Date.now()
-      this.nextSendTime = Math.max(Date.now(),this.nextSendTime) + this.minSendInterval
-      if(timeTillSend > 0){
-        await new Promise(res => setTimeout(res,timeTillSend))
-      }
-
-      // Finally make the actual call
-      return got(path.href,options).catch(err => {
-        console.log(Object.entries(err))
-        // throw new Error(`${err}\n    at ${blame.getFunctionName()||'<anonymous>'} (${blame.getFileName()}:${blame.getLineNumber()})`)
-      })
-    }).bind(this))
-
-    // Update the rateLimitRemaining
-    this.RateLimitRemaining = response.headers['x-rate-limit-remaining']
-    // console.log(Math.floor(this.RateLimitRemaining),Number(response.headers['x-request-cost']).toFixed(3))
-    // Turn my links string into a useful object
-    let links = this.parseLink(response.headers.link)
-
-    // Paginate recursivly if need to paginate
-    if(links && links.current.page == 1){
-      let responses = await Promise.all(this.range(+links.last.page).map(page => {
-        let path = new url.URL(links.current.path)
-        path.searchParams.set('page',page)
-
-        return this.call(blame,path.href)
-      }))
+      // The waiting a second
+      await new Promise(res => setTimeout(res,settings.checkStatusInterval))
+      // See what the situation is now
       try{
-        response.body = response.body.concat(...responses.map(res => res.body))
-      } catch (e){
-        throw new Error('Assumption that paginating body is always an array, was wrong')
+        let response = await got(url.resolve(baseUrl,'/api/v1/users/self'),{
+          method: 'HEAD',
+          headers:{
+            Authorization: 'Bearer '+settings.apiToken
+          }
+        })
+        rateLimitRemaining = response.headers['x-rate-limit-remaining']
+      } catch(e){
+        // We couldn't even make the check
+        console.log('We\'re in trouble')
+      }
+      if(rateLimitRemaining === undefined){
+        throw new Error("There was no x-rate-limit-remaining header")
       }
     }
-    return response
-  }
-  parseLink(str){
-    if(str){
-      return str.split(',')
-        .map(str => str.match(/<(.*?page=(\d+).*?)>.*?"(.*?)"/))
-        .reduce((obj,elm) => {obj[elm[3]] = {path:elm[1],page:elm[2]}; return obj},{})
-    }
-  }
-}
 
-function findblame(){
-  let s = stack(), i = 0
-  while(s[i].getFileName() == __filename){i++}
-  return s[i]
-}
+    // Make sure our calls are staggered at least a little bit, 
+    // like 10 milliseconds, so that the first calls doesn't spam the server
+    var timeTillSend = nextSendTime - Date.now()
+    nextSendTime = Math.max(Date.now(),nextSendTime) + settings.minSendInterval
+    if(timeTillSend > 0){
+      await new Promise(res => setTimeout(res,timeTillSend))
+    }
 
-module.exports = function(){
-  let origFolder = path.dirname(stack()[1].getFileName())
-  var canvas = new Canvas(origFolder,...arguments)
-  function WrapWrapper(){
-    let args = [...arguments], blame = findblame()
-    if(typeof args[args.length-1] == 'function'){
-      // expecting a callback
-      if(args.length == 2){
-        // they didn't give us any options
-        args[2] = args[1]
-        args[1] = {}
-      }
-      return util.callbackify(canvas.call).call(canvas,blame,...args)
-    }
-    return canvas.call.call(canvas,blame,...args)
-  }
-  return new Proxy(new Function(),{
-    apply: function(target, thisArg, argumentsList){
-      return WrapWrapper.apply(null,argumentsList)
-    },
-    get: function(target,prop){
-      if(['get','post','put','patch','head','delete'].includes(prop)){
-        return WrapWrapper
-      } else {
-        return Reflect.get(...arguments);
-      }
-    }
+    // Finally make the actual call
+    return got(path.href,options).catch(err => {
+      // editing the err so that the call stack points to the owner
+      throw combineErrors(err,blame)
+    })
   })
+
+  // Update the rateLimitRemaining
+  rateLimitRemaining = response.headers['x-rate-limit-remaining']
+  // console.log(Math.floor(this.RateLimitRemaining),Number(response.headers['x-request-cost']).toFixed(3))
+  // Turn my links string into a useful object
+  let links = parseLink(response.headers.link)
+
+  // Paginate recursivly if need to paginate
+  if(links && links.current.page == 1){
+    let responses = await Promise.all(Array(links.last.page-1).fill().map((n,i) => i+2).map(page => {
+      let path = new url.URL(links.current.path)
+      path.searchParams.set('page',page)
+      return call(blame,path.href)
+    }))
+    try{
+      response.body = response.body.concat(...responses.map(res => res.body))
+    } catch (e){
+      throw new Error('Assumption that paginating body is always an array, was wrong')
+    }
+  }
+
+  return response
 }
+
+// Mashes the callstack into the got error
+function combineErrors(err,stack){
+  let i = 0, str = err.toString()+'\n'
+  while(stack[i].getFileName() == __filename){i++}
+  for(;i < stack.length; i++){
+    str+= `    at ${stack[i].getFunctionName() || 'anonymous'} (${stack[i].getFileName()}:${stack[i].getLineNumber()})\n`
+  }
+  err.stack = str
+  return err
+}
+
+// Parses canvas's crazy pagination method
+function parseLink(str){
+  if(str){
+    return str.split(',')
+      .map(str => str.match(/<(.*?page=(\d+).*?)>.*?"(.*?)"/))
+      .reduce((obj,elm) => {obj[elm[3]] = {path:elm[1],page:elm[2]}; return obj},{})
+  }
+}
+
+// Is responsible for our making our crazy function signiture
+function canvas(){
+  if(!settings.apiToken){
+    throw new Error('Canvas API Token was not set')
+  }
+  let args = [...arguments], blame = stack()
+  if(typeof args[args.length-1] == 'function'){
+    // expecting a callback
+    if(args.length == 2){
+      // they didn't give us any options
+      args[2] = args[1]
+      args[1] = {}
+    }
+    return util.callbackify(call).call(this,blame,...args)
+  }
+  return call.call(this,blame,...args)
+}
+
+// Just cause proxies are so cool
+module.exports = new Proxy(new Function(),{
+  apply: function(target, thisArg, argumentsList){
+    return canvas.apply(null,argumentsList)
+  },
+  get: function(target,prop){
+    if(['get','post','put','patch','head','delete'].includes(prop)){
+      return canvas.bind({method:prop})
+    } else {
+      return Reflect.get(...arguments);
+    }
+  },
+  set: function(target,prop,value){
+    if(Object.keys(settings).includes(prop)){
+      if(typeof settings[prop] == typeof value){
+        settings[prop] = value
+        if(prop == 'callLimit'){
+          if(queue.queue == 0){
+            queue = promiseLimit(value)
+          } else {
+            throw new Error("Can't change the queue size while the queue is in operation")
+          }
+        } else if(prop == 'domain'){
+          baseUrl = `https://${settings.domain}.instructure.com`
+        }
+      } else {
+        throw new Error(prop+" was expecting a "+typeof settings[prop]+" instead of a "+typeof value)
+      }
+    } else {
+      throw new Error("I don't have a setting with the name "+prop)
+    }
+  }
+})
